0b568abcc6db3675ed108167f7a4c581703d32bc
PaperAnchor <zach@paperanchor.ca>
2016-02-09 21:08:58 -0700

    Fixed several layout issues

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,30 +1,30 @@
-class CategoriesController < ApplicationController  
-  
+class CategoriesController < ApplicationController
+
   def index
    @categories = Category.all
   end
-  
+
   def new
     @category = Category.new
   end
-  
+
   def create
     @category = Category.new post_params
-    
+
     if @category.save
       redirect_to :categories, notice: "Your Category Has Been Created!"
     else
       render 'new', notice: "Oh no! That didn't work!"
     end
   end
-  
+
   def show
     @categories = Category.all
   end
-  
+
   def edit
   end
-  
+
   def update
     if @category.update post_params
       redirect_to @category, notice: "Yay! The category info has been updated!"
@@ -32,17 +32,12 @@ class CategoriesController < ApplicationController
       render 'edit'
     end
   end
-  
-  def destroy
-    @category.destroy
-    redirect_to businesses_path
-  end
-  
+
   private
-  
+
   def post_params
     params.require(:category).permit(:id)
   end
-  
-  
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   devise_scope :user do
     get "sign_in" => "devise/sessions#new"
   end
-  
-  
+
+
   resources :businesses do
     collection do
       get 'search'
     end
   end
-  
+
   resources :categories
-  resources :admin
   resources :contacts, only: [:new, :create]
-  
+
   get ':id', to: 'categories#show', as: :id
-  
-  delete 'admin' => 'categories#destroy'
-  
-  get "contact" => "contacts#new"
+
   get 'welcome/index'
   root 'welcome#index'
-  
+
   get '*path' => redirect('/')
 end
